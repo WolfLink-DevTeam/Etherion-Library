f2c9fca414a6b234ceb80387969632446f7bad6e
MikkoAyaka <3401286177@qq.com>
2023-03-16 22:32:10 +0800

    添加了一些紫微斗数的基础内容

---
+++ src/main/kotlin/enums/purplestar/PalaceType.kt
@@ -0,0 +1,9 @@
+package enums.purplestar
+
+import java.util.StringJoiner
+
+enum class PalaceType(val chineseName : String) {
+    Ming("命宫"),XiongDi("兄弟宫"),FuQi("夫妻宫"),ZiNu("子女宫"),
+    CaiBo("财帛宫"),JiE("疾厄宫"),QianYi("迁移宫"),JiaoYou("交友宫"),
+    GuanLu("官禄宫"),TianZhai("田宅宫"),FuDe("福德宫"),FuMu("父母宫")
+}
