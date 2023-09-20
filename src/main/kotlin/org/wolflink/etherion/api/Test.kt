edeaabd5c70bc85e85e74abefdc89daba026422f
MikkoAyaka <3401286177@qq.com>
2023-09-20 15:55:28 +0800

    封装 loadResource 方法用于加载项目路径中的资源文件

--- src/main/kotlin/org/wolflink/etherion/api/Test.kt
+++ src/main/kotlin/org/wolflink/etherion/api/Test.kt
@@ -4,10 +4,6 @@ import java.io.InputStreamReader
 object Test {
     @JvmStatic
     fun main(args: Array<String>) {
-        val inputStream = javaClass.classLoader.getResourceAsStream("data/SolarDeviation")
-        if (inputStream != null) {
-            val reader = InputStreamReader(inputStream)
-            println(reader.readText())
-        }
+
     }
 }
--- src/main/kotlin/org/wolflink/etherion/api/utils/ResourceUtil.kt
+++ src/main/kotlin/org/wolflink/etherion/api/utils/ResourceUtil.kt
@@ -1,4 +1,21 @@
 package org.wolflink.etherion.api.utils
 
-class ResourceUtil {
+import java.io.InputStreamReader
+
+object ResourceUtil {
+    /**
+     * 加载项目资源文件
+     * @param path  资源文件相对路径，例如 data/SolarDeviation
+     */
+    fun loadResource(path: String): String {
+        val inputStream = javaClass.classLoader.getResourceAsStream("")
+        var result = ""
+        if (inputStream != null) {
+            val reader = InputStreamReader(inputStream)
+            result = reader.readText()
+            reader.close()
+            inputStream.close()
+        }
+        return result
+    }
 }
