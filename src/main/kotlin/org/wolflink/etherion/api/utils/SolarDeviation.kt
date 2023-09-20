ac407650fa70269e4ed0381db35808cc7348d5f3
MikkoAyaka <3401286177@qq.com>
2023-09-20 16:43:19 +0800

    删除原有的 SolarDeviationConfig 将真太阳时数据封装到 SolarDeviation 中

--- src/main/kotlin/org/wolflink/etherion/api/utils/SolarDeviation.kt
+++ src/main/kotlin/org/wolflink/etherion/api/utils/SolarDeviation.kt
@@ -1,16 +1,21 @@
 package org.wolflink.etherion.api.utils
 
-import org.wolflink.etherion.api.configs.SolarDeviationConfig
-
 /**
  * 真太阳时偏差校准
  */
 object SolarDeviation {
 
+    private val deviationMap: Map<Pair<Int,Int>,Int> =
+        ResourceUtil.loadResource("data/SolarDeviation").split('\n').associate { line ->
+            val args = line.split(' ')
+            val (month,day) = args[0].split('-')
+            (month.toInt() to day.toInt()) to args[1].toInt()
+        }
+
     // 获取偏差时间(秒)
     fun getDeviation(month : Int,day : Int) : Int
     {
-        return SolarDeviationConfig.solarDeviationMap[month to day] ?: throw IllegalArgumentException("无法根据给定月日找到匹配的偏差值。月：$month，日：$day")
+        return deviationMap[month to day] ?: throw IllegalArgumentException("无法根据给定月日找到匹配的偏差值。月：$month，日：$day")
     }
 
 }
