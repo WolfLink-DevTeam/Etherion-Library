2090c56af58e9b68e51230d0918504c35c69c17f
MikkoAyaka <3401286177@qq.com>
2023-10-15 23:32:40 +0800

    补充六神和六亲关系

---
+++ src/main/kotlin/org/wolflink/etherion/lib/enums/sixren/SixGods.kt
@@ -0,0 +1,10 @@
+package org.wolflink.etherion.lib.enums.sixren
+
+enum class SixGods(val chineseName: String) {
+    XuanWu("玄武"),
+    ZhuQue("朱雀"),
+    GouChen("勾陈"),
+    TengShe("螣蛇"),
+    BaiHu("白虎"),
+    QingLong("青龙")
+}
---
+++ src/main/kotlin/org/wolflink/etherion/lib/enums/sixren/SixRelation.kt
@@ -0,0 +1,9 @@
+package org.wolflink.etherion.lib.enums.sixren
+
+enum class SixRelation(val chineseName: String) {
+    ZiSun("子孙"),
+    FuMu("父母"),
+    GuanGui("官鬼"),
+    XiongDi("兄弟"),
+    QiCai("妻财")
+}
